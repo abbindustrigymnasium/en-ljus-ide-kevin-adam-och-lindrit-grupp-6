78b01d80114ae0a552ef5ded21d8949d5acca1f5
abblinkox <43879885+abblinkox@users.noreply.github.com>
2018-11-21 14:04:16 +0100

    App, första design
    den första designen på våran app

---
+++ designapp/app/components/screen2/Component7.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { 
+	StyleSheet,
+	View,
+	Text,
+	Switch,
+} from 'react-native';
+
+
+export default class Component7 extends React.Component {
+
+    constructor(props) {
+    
+        super(props);
+
+        this.state = {
+            switchValue: false,
+        }
+
+    }
+
+
+    render() {
+
+        if (!this.props.visible) {
+            return false;
+        }
+        
+
+        return (
+
+            <View 
+                style={styles.component}
+            >
+
+                <View style={styles.layouts}>
+
+                	<View style={styles.layout1}>
+
+                		<View style={styles.itemcontainer1}>
+
+                			<View style={styles.itemcontainer1Inner}>
+
+                                <View style={styles.item1}>
+										<Switch 
+											value={this.state.switchValue}
+											onValueChange={(val) => this.setState({ switchValue : val })}
+										/>
+									</View>
+
+                			</View>
+
+                		</View>
+
+                	</View>
+                	
+                </View>
+
+            </View>
+            
+        );
+
+    }
+
+}
+
+const styles = StyleSheet.create({
+    
+	component: {
+	    width: '100%',
+	    flexDirection: 'row',
+	    paddingLeft: 7.5,
+	    paddingRight: 7.5,
+	    paddingTop: 7.5,
+	    paddingBottom: 7.5,
+	},
+	
+	layouts: {
+	    flexDirection: 'row',
+	    flexWrap: 'wrap',
+	},
+	
+	layout1: {
+	    width: '100%',
+	    height: 109.5,
+	},
+	
+	itemcontainer1: {
+	    width: '100%',
+	    height: '100%',
+	    paddingTop: 7.5,
+	    paddingBottom: 7.5,
+	    paddingLeft: 7.5,
+	    paddingRight: 7.5,
+	},
+	
+	itemcontainer1Inner: {
+	    width: '100%',
+	    height: '100%',
+	    position: 'relative',
+	    alignItems: 'center',
+	    justifyContent: 'center',
+	},
+	
+	item1: {
+	    width: '100%',
+	    height: '100%',
+	    alignItems: 'center',
+	    justifyContent: 'center',
+	    overflow: 'hidden',
+	    borderStyle: 'solid',
+	    borderWidth: 0,
+	    backgroundColor: 'rgba(239,239,239,1)',
+	},
+	
+});
---
+++ designapp/app/components/screen3/Screen3.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import { 
+    StyleSheet,
+    View,
+} from 'react-native';
+
+import Component12 from './Component12';
+import Component13 from './Component13';
+import Component14 from './Component14';
+import Component15 from './Component15';
+import Component16 from './Component16';
+
+export default class Screen3 extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            Component12Visible: true,
+            Component13Visible: true,
+            Component14Visible: true,
+            Component15Visible: true,
+            Component16Visible: true,
+        }
+
+    }
+
+    toggleComponent(component = false) {
+
+        if (!component) {
+            return false;
+        }
+
+        let prop = component + 'Visible';
+        let val  = this.state[prop];
+        if (typeof val === 'undefined') {
+            return false;
+        }
+
+        this.setState({
+            [prop]: val === true ? false : true
+        })
+
+        return true;
+
+    }
+
+    hideComponent(component = false) {
+
+        if (!component) {
+            return false;
+        }
+
+        let prop = component + 'Visible';
+
+        this.setState({
+            [prop]: false
+        })
+
+        return true;
+
+    }
+
+    showComponent(component = false) {
+
+        if (!component) {
+            return false;
+        }
+
+        let prop = component + 'Visible';
+
+        this.setState({
+            [prop]: true
+        })
+
+        return true;
+
+    }
+
+    render() {
+        return (
+
+            
+            <View style={styles.container}>
+
+                <View style={styles.screencontainer}>
+
+                    <View style={styles.screencontainerInner}>
+
+                        <Component12 
+                            navigation={this.props.navigation}
+                            toggleComponent={ (component) => this.toggleComponent(component) }
+                            hideComponent={ (component) => this.hideComponent(component) }
+                            showComponent={ (component) => this.showComponent(component) }
+                            visible={ this.state.Component12Visible }
+                        />
+                        <Component13 
+                            navigation={this.props.navigation}
+                            toggleComponent={ (component) => this.toggleComponent(component) }
+                            hideComponent={ (component) => this.hideComponent(component) }
+                            showComponent={ (component) => this.showComponent(component) }
+                            visible={ this.state.Component13Visible }
+                        />
+                        <Component14 
+                            navigation={this.props.navigation}
+                            toggleComponent={ (component) => this.toggleComponent(component) }
+                            hideComponent={ (component) => this.hideComponent(component) }
+                            showComponent={ (component) => this.showComponent(component) }
+                            visible={ this.state.Component14Visible }
+                        />
+                        <Component15 
+                            navigation={this.props.navigation}
+                            toggleComponent={ (component) => this.toggleComponent(component) }
+                            hideComponent={ (component) => this.hideComponent(component) }
+                            showComponent={ (component) => this.showComponent(component) }
+                            visible={ this.state.Component15Visible }
+                        />
+                        <Component16 
+                            navigation={this.props.navigation}
+                            toggleComponent={ (component) => this.toggleComponent(component) }
+                            hideComponent={ (component) => this.hideComponent(component) }
+                            showComponent={ (component) => this.showComponent(component) }
+                            visible={ this.state.Component16Visible }
+                        />
+
+                    </View>
+
+                </View>
+
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+    },
+    
+	screencontainer: {
+	    backgroundColor: 'rgba(255,255,255,1)',
+	    flex: 1,
+	},
+	
+	screencontainerInner: {
+	    flex: 1,
+	},
+	
+});
